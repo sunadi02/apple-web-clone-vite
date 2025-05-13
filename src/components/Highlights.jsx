2d36716bdcd4d42d7739002f881f84296137dab7
sunadi amarasinghe <sunadiamarasinghe@gmail.com>
2025-05-13 12:36:29 +0530

    Updated styles and fixes

--- src/components/Highlights.jsx
+++ src/components/Highlights.jsx
@@ -1,8 +1,32 @@
-
+import { useGSAP } from "@gsap/react"
+import gsap from "gsap"
+import { watchImg } from "../utils"
 
 const Highlights = () => {
+  useGSAP(() => {
+    gsap.to('#title', {
+      opacity:1, y:0
+    })
+    gsap.to('.link', {
+      opacity:0
+    })
+    
+  },[])
   return (
-    <div>Highlights</div>
+    <section id="highlights" className="w-screen overflow-hidden h-full common-padding bg-[#101010] ">
+      <div className="screen-max-width">
+        <div className="mb-12 w-full items-end">
+          <h1 id="title" className="section-heading">Get the highlights.</h1>
+
+          <div className="flex flex-wrap items-end gap-5">
+            <p className="link">
+              Watch the film
+              <img src={watchImg} alt="watch" className="ml-2"/>
+            </p>
+          </div>
+        </div>
+      </div>
+    </section>
   )
 }
 
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,14 +3,13 @@ import { navLists } from '../constants';
 
 const Navbar = () => {
   return (
-    <header className="w-full mt-5 sm:px-10 px-5 flex justify-between items-center">
-
-      <nav className="flex py-0 w-full screen-max-width">
+    <header className="w-full py-5 sm:px-10 px-5 flex justify-between items-center">
+      <nav className="flex w-full screen-max-width">
         <img src={appleImg} alt="Apple" width={14} height={18} />
 
-        <div className="flex flex-1 justify-center max-sm:hidden gap-x-16">
+        <div className="flex flex-1 justify-center max-sm:hidden ">
           {navLists.map((nav) => (
-            <div key={nav} className="px-5 text-sm cursor-pointer text-gray-300 hover:text-white transition-all">
+            <div key={nav} className="px-5 text-sm cursor-pointer text-[#86868b] hover:text-white transition-all">
               {nav}
             </div>
           ))}
