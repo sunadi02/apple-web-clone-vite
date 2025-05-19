fa779962f4c324b42248442ad49bd24acdac89a9
sunadi amarasinghe <sunadiamarasinghe@gmail.com>
2025-05-19 19:06:38 +0530

    Added GSAP animations and improved video playback logic

--- src/components/Highlights.jsx
+++ src/components/Highlights.jsx
@@ -1,6 +1,8 @@
 import { useGSAP } from "@gsap/react"
 import gsap from "gsap"
-import { watchImg } from "../utils"
+import { rightImg, watchImg } from "../utils"
+
+import VideoCarousel from "./VideoCarousel"
 
 const Highlights = () => {
   useGSAP(() => {
@@ -8,14 +10,14 @@ const Highlights = () => {
       opacity:1, y:0
     })
     gsap.to('.link', {
-      opacity:0
+      opacity:1, y:0, duration:1, stagger:0.25
     })
     
   },[])
   return (
     <section id="highlights" className="w-screen overflow-hidden h-full common-padding bg-[#101010] ">
       <div className="screen-max-width">
-        <div className="mb-12 w-full items-end">
+        <div className="mb-12 w-full  md:flex items-end justify-between">
           <h1 id="title" className="section-heading">Get the highlights.</h1>
 
           <div className="flex flex-wrap items-end gap-5">
@@ -23,8 +25,15 @@ const Highlights = () => {
               Watch the film
               <img src={watchImg} alt="watch" className="ml-2"/>
             </p>
+            <p className="link">
+              Watch the event
+              <img src={rightImg} alt="right" className="ml-2"/>
+            </p>
+
           </div>
         </div>
+
+      <VideoCarousel/>
       </div>
     </section>
   )
