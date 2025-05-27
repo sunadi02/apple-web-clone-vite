c22dfcb2d3516aef5b5f50aa2ba9e34ff5a3f58a
sunadi amarasinghe <sunadiamarasinghe@gmail.com>
2025-05-27 22:28:26 +0530

    Added ModelView component with OrbitControls, Lights, and iPhone model

--- src/components/Model.jsx
+++ src/components/Model.jsx
@@ -4,6 +4,10 @@ import { useRef, useState } from "react";
 import ModelView from "./ModelView";
 import { yellowImg } from "../utils";
 import * as THREE from 'three';
+import  { Canvas }  from "@react-three/fiber";
+import { models, sizes } from "../constants";
+import { View } from "@react-three/drei";
+
 
 
 const Model = () => {
@@ -50,6 +54,50 @@ const Model = () => {
                         item={model}
                         size={size}
                     />
+                    <ModelView
+                        index={2}
+                        groupRef={large}
+                        gsapType="view2"
+                        controlRef= {cameraControlLarge}
+                        setRotationState={setLargeRotation}
+                        item={model}
+                        size={size}
+                    />
+                    <Canvas
+                    className="w-full h-full" 
+                    style={{
+                        position: 'fixed',
+                        top:0,
+                        bottom:0,
+                        left:0,
+                        right:0,
+                        overflow:'hidden'
+                    }}
+                    eventSource = {document.getElementById('root')}
+                    >
+                        <View.Port/>
+                    </Canvas>
+                    
+                </div>
+                <div className="mx-auto w-full">
+                    <p className="text-sm font-light text-center mb-5">{model.title}</p>
+                    <div className="flex-center">
+                        <ul className="color-container">
+                            {models.map((item, i) => (
+                                <li key = {i} className="w-6 h-6 rounded-full mx-2 cursor-pointer" style={{backgroundColor:item.color[0] }} onClick={() => setModel(item)}/>
+                            ))}
+                        </ul>
+
+                        <button className="size-btn-container">
+                            {sizes.map(({ label, value }) => (
+                                <span key={label} className="size-btn cursor-pointer" 
+                                style={{backgroundColor: size === value ? 'white' : 'transparent', 
+                                color : size === value ? 'black' : 'white'}} onClick={() => setSize(value)} >
+                                    {label}
+                                </span>
+                            ))}
+                        </button>
+                    </div>
                 </div>
             </div>
         </div>
