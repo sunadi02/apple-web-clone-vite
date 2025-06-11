c53387c3223da65213d0b751c95a1af941061f3f
sunadi amarasinghe <sunadiamarasinghe@gmail.com>
2025-06-11 20:14:11 +0530

    Integrated HowItWorks section with updated GPU info and animations

--- src/components/HowItWorks.jsx
+++ src/components/HowItWorks.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { chipImg } from '../utils'
+import { chipImg, frameImg, frameVideo } from '../utils'
 import { useGSAP } from '@gsap/react'
 import gsap from 'gsap'
 
@@ -22,6 +22,28 @@ const HowItWorks = () => {
         <div className='screen-max-width'>
             <div id='chip' className='flex-center w-full my-20'>
                 <img src={chipImg} alt="chip" width={180} height={180} />
+            </div>
+
+            <div className='flex flex-col items-center'>
+                <h2 className='hiw-title'>
+                    A17 Pro chip.
+                    <br /> A monster win for gaming.
+                </h2>
+
+                <p className='hiw-subtitle'>
+                    It's here. The biggest redesign in the history of Apple GPUs.
+                </p>
+            </div>
+
+            <div className='mt-10 md:mt-20 mb-14' >
+                <div className='relative h-full flex-center'>
+                    <div className='overflow-hidden'>
+                        <img src={frameImg} alt="frame" className='bg-transparent relative z-10' />
+                    </div>
+                    <div className='hiw-video'>
+                            <video src={frameVideo} type='video/mp4'></video>
+                        </div>
+                </div>
             </div>
         </div>
     </section>
